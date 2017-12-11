c81c4785ef9b08991cf6d5b6f774efdf4620f344
Derek Covey <derek.covey@gmail.com>
2017-12-11 10:44:36 -0800

    Remove LinkedList wrapper class

--- add_two_numbers/add_two_numbers.py
+++ add_two_numbers/add_two_numbers.py
@@ -3,13 +3,9 @@ class Node(object):
         self.data=data
         self.next_node = next_node
 
-class LinkedList(object):
-    def __init__(self, head):
-        self.head = head
-
     def __str__(self):
         string = ""
-        node = self.head
+        node = self
         while not node == None:
             string += str(node.data)
             string += " -> "
@@ -34,8 +30,8 @@ class Solution(object):
             Output: (7 -> 0 -> 8)
             Explanation: 342 + 465 = 807
         """
-        node1 = l1.head
-        node2 = l2.head
+        node1 = l1
+        node2 = l2
 
         if node1 == None and node2 == None:
             # Catch base case for recursive call
@@ -67,26 +63,21 @@ class Solution(object):
             carry = False
 
         num_sum = num_sum % 10
-        new_l1 = LinkedList(l1.head.next_node)
-        new_l2 = LinkedList(l2.head.next_node)
+        new_l1 = node1.next_node
+        new_l2 = node2.next_node
 
 
         new_node = Node(num_sum, self.add_two_numbers(new_l1, new_l2, carry))
 
         return new_node
 
-    def add_two_numbers_output_list(self, l1, l2):
-        return LinkedList(self.add_two_numbers(l1, l2))
-
-
-
 
 
 
 sol = Solution()
-l1 = LinkedList(Node(2, Node(4, Node(3, None))))
-l2 = LinkedList(Node(5, Node(6, Node(4, None))))
+l1 = Node(2, Node(4, Node(3, None)))
+l2 = Node(5, Node(6, Node(4, None)))
 
-sum_list = sol.add_two_numbers_output_list(l1, l2)
+sum_list = sol.add_two_numbers(l1, l2)
 
 print(sum_list)
