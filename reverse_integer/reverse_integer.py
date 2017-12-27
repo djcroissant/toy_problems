cae52028ad89c5ce82ecc281ae7be8405025a367
Derek Covey <derek.covey@gmail.com>
2017-12-27 11:45:25 -0800

    Refactor max length check

--- reverse_integer/reverse_integer.py
+++ reverse_integer/reverse_integer.py
@@ -38,19 +38,16 @@ class Solution(object):
 
         Refactor to check for length
         """
-        if abs(num_in) > 2**32 / 2 - 1 or num_in == 0:
-            return 0
-
         flag = 1
         if num_in < 0:
             flag = -1
             num_in *= -1
 
-        num_out = int(str(num_in)[::-1] * flag
+        num_out = int(str(num_in)[::-1]) * flag
 
-        if abs(num_out) > 2**32 / 2 - 1:
+        maxInt = 2**32 / 2 - 1
+        if abs(num_out) > maxInt:
             return 0
-
 
         return num_out
 
@@ -62,6 +59,7 @@ print(sol.reverse_integer(-123))
 print(sol.reverse_integer(120))
 print(sol.reverse_integer(-120))
 print(sol.reverse_integer(9))
+print(sol.reverse_integer(0))
 print(sol.reverse_integer(-9))
 print(sol.reverse_integer(2**32))
 print(sol.reverse_integer(2**32 + 1))
