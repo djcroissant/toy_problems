22133cbec8defb18f2ef4e117dc7ab616d0db0eb
Derek Covey <derek.covey@gmail.com>
2018-05-18 15:00:41 -0700

    Create solution for valid parentheses problem

---
+++ valid_parentheses/valid_parentheses.py
@@ -0,0 +1,42 @@
+class Solution():
+    """
+    input: a string containing only parentheses (i.e. (, [, {, }, ], ))
+    output: true if:
+        * each open parentheses has a closing parentheses
+        * parentheses are closed in the same order they were opened
+
+    example:
+        parens = '()[]{}'
+        ==> True
+
+        parens = '([)]'
+        ==> False
+
+        parens = '(]'
+        ==> False
+    """
+
+    def valid_parentheses(self, parens):
+        if parens == '':
+            return True
+        open_sequence = []
+        open = ['(', '[', '{']
+        close = [')', ']', '}']
+        for c in parens:
+            if c in open:
+                open_sequence.append(c)
+            elif c in close:
+                opener = open_sequence.pop()
+                open_index = open.index(opener)
+                close_index = close.index(c)
+                if open_index != close_index:
+                    return False
+
+        if not open_sequence:
+            return True
+
+# Testing:
+sol = Solution()
+print(sol.valid_parentheses('()[]{}'))
+print(sol.valid_parentheses('([)]'))
+print(sol.valid_parentheses('(]'))
